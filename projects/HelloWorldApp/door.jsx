fa4ea9eb8dab5f39c64e9617ff5d8000129ee5e0
Sakti Swarupa Mallick <152704778+saktiswarupamallick@users.noreply.github.com>
2024-01-26 18:29:39 +0000

    #

--- projects/HelloWorldApp/door.jsx
+++ projects/HelloWorldApp/door.jsx
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function door() {
+export default function door(props) {
+  const {color} = props
   return (
     <View style={styles.container}>
       <Text>hello!</Text>
